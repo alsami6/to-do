1585d76aeb379e2d2c72fe137b5c2e4a69f634ae
Al <alsami1209@gmail.com>
2024-04-01 14:33:41 +0600

    need to learn new thing

---
+++ main.js
@@ -0,0 +1,6 @@
+const todoFrom = document.querySelector('#todo-from')
+const totalTasks = document.querySelector('#total-tasks')
+const remaningTasks = document.querySelector('#remaning-tasks')
+const completedTasks = document.querySelector('#completed-tasks')
+// const totalTasks = document.querySelector('#total-tasks')
+// const totalTasks = document.querySelector('#total-tasks')
