246fb149ed6a713508706620ee42bb309fc5f7cb
Al <alsami1209@gmail.com>
2024-04-02 16:20:34 +0600

    need to learn new thing

--- main.js
+++ main.js
@@ -4,3 +4,4 @@ const remaningTasks = document.querySelector('#remaning-tasks')
 const completedTasks = document.querySelector('#completed-tasks')
 // const totalTasks = document.querySelector('#total-tasks')
 // const totalTasks = document.querySelector('#total-tasks')
+// I need to learn Local storage to contiune
